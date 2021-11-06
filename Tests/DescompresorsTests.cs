cf78771c94f8180b77ce8386e7f1e36765108982
caraplana1 <cristianlandaez@gmail.com>
2021-11-06 19:57:10 -0400

    Unrar Tests completed

--- Tests/DescompresorsTests.cs
+++ Tests/DescompresorsTests.cs
@@ -1,10 +1,12 @@
 using System.IO;
 using Xunit;
 using ComicConverter;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Tests
+namespace DescompresorsTests
 {
-    public class DescompresorsTests
+    public class UnrarTests
     {
         [Fact]
         public void UnrarTest()
@@ -26,7 +28,50 @@ namespace Tests
                 Assert.True(false);
 
             extractedFiles = Directory.GetFiles(outputPath);
+            Directory.Delete(outputPath, true);
             Assert.True(extractedFiles.Length > 0);
+        }
+
+        [Fact]
+        public void RarFileNotExists()
+        {
+            string filePath = "file.txt";
+            string output = "Folder";
+
+            Assert.Throws<FileNotFoundException>(() => Descompresor.UnRar(filePath, output));
+        }
+
+        [Fact]
+        public void FileIsNotRar()
+        {
+            string filePath = "../../../Samples/File.test";
+
+            Assert.Throws<System.FormatException>(() => Descompresor.UnRar(filePath, "Folder"));
+        }
+
+        [Fact]
+        public void EmptyOutputFolder()
+        {
+            string filePath = "../../../Samples/CROSSED Wish you were here.cbr";
+            List<string> filesExtracted;
+
+            Descompresor.UnRar(filePath, "Folder");
+            Descompresor.UnRar(filePath);
+
+            filesExtracted = Directory.GetFiles(".").ToList<string>();
+
+            for (int i = 0; i < filesExtracted.Count; i++)
+                filesExtracted[i] = filesExtracted[i][2..];
+
+            foreach (var file in Directory.GetFiles("Folder").ToList<string>())
+            {
+                if (!filesExtracted.Contains(file[7..]))
+                    Assert.True(false);
+                else
+                    File.Delete(@".\\" + file[7..]);
+            }
+
+            Directory.Delete("Folder", true);
         }
     }
 }
