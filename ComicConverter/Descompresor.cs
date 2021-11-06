cdd8f4866418844b68572e95206e9e65929894bf
caraplana1 <cristianlandaez@gmail.com>
2021-11-06 17:55:39 -0400

    Unrar Method Created

--- ComicConverter/Descompresor.cs
+++ ComicConverter/Descompresor.cs
@@ -1,9 +1,32 @@
-﻿using System;
-using SharpCompress;
+﻿using System.IO;
+using SharpCompress.Archives.Rar;
+using SharpCompress.Archives;
+using SharpCompress.Common;
+using System.Linq;
 
 namespace ComicConverter
 {
-    public class Converter
+    public static class Descompresor
     {
+        public static void UnRar(string filePath, string outputDir = ".output")
+        {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException("The file doesn't exists");
+            else if (!RarArchive.IsRarFile(filePath))
+                throw new System.FormatException("The file is not a rar file");
+
+           Directory.CreateDirectory(outputDir);
+
+           using RarArchive archive = RarArchive.Open(filePath);
+           
+           foreach (var entry in archive.Entries.Where(a => !a.IsDirectory))
+           {
+                entry.WriteToDirectory(outputDir, new ExtractionOptions()
+                {
+                    ExtractFullPath = false,
+                    Overwrite = true
+                });
+           }
+        }
     }
 }
