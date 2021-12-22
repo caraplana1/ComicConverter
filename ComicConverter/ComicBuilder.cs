8d39668e2510a7bd5902dc0cf665b656e44af9a2
caraplana1 <cristianlandaez@gmail.com>
2021-12-22 11:49:52 -0400

    ComicBuilder class created

---
+++ ComicConverter/ComicBuilder.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ComicConverter
+{
+	public static class ComicBuilder
+	{
+		/// <summary>
+		/// Creates zip file but with cbz extension
+		/// </summary>
+		/// <param name="imagesPath">Arrays of paths to images to include in the file</param>
+		/// <param name="fileName">Name of the final document. Dont need to add the extension name</param>
+		public static void CreateCBZ(string[] imagesPath, string fileName) => throw new NotImplementedException();
+	}
+}
