e48a65e3f2ceca7130f31b55631148ce7b7d733d
caraplana1 <cristianlandaez@gmail.com>
2021-12-23 01:03:17 -0400

    Cbz creator completed

--- ComicConverter/ComicBuilder.cs
+++ ComicConverter/ComicBuilder.cs
@@ -1,5 +1,10 @@
 using System;
+using System.Text;
+using System.IO;
 using System.Linq;
+using SharpCompress.Common;
+using SharpCompress.Archives;
+using SharpCompress.Archives.Zip;
 using System.Collections.Generic;
 
 namespace ComicConverter
@@ -9,8 +14,29 @@ namespace ComicConverter
 		/// <summary>
 		/// Creates zip file but with cbz extension
 		/// </summary>
-		/// <param name="imagesPath">Arrays of paths to images to include in the file</param>
+		/// <param name="imagesPath">Arrays of paths to  the images to include in the file</param>
 		/// <param name="fileName">Name of the final document. Dont need to add the extension name</param>
-		public static void CreateCBZ(string[] imagesPath, string fileName) => throw new NotImplementedException();
+		public static void CreateCBZ(string[] imagesPaths, string fileName)
+		{
+			DirectoryInfo dir = Directory.CreateDirectory(".hidden");
+			dir.Attributes = FileAttributes.Directory | FileAttributes.Hidden;
+
+			if (!imagesPaths.Any(f => File.Exists(f))) 
+				throw new IOException("There is no file to add");
+
+			foreach (var image in imagesPaths)
+			{
+				var finalFilePath = image.Replace(@"\", "/").Split('/');
+				File.Copy(image, $"{dir.Name}/{finalFilePath.Last()}", true);
+			}
+
+			using (var archive = ZipArchive.Create())
+			{
+				archive.AddAllFromDirectory(dir.Name);
+				archive.SaveTo($"{fileName}.cbz", CompressionType.Deflate);
+			}
+
+			dir.Delete(true);
+		}
 	}
 }
