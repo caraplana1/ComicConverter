f589d488068b404c56b417b0dc3eb81b71e8068c
caraplana1 <cristianlandaez@gmail.com>
2021-12-27 22:31:25 -0400

    method refactored to created hidden directory to compress

--- ComicConverter/ComicBuilder.cs
+++ ComicConverter/ComicBuilder.cs
@@ -18,17 +18,7 @@ namespace ComicConverter
 		/// <param name="fileName">Name of the final document. Dont need to add the extension name</param>
 		public static void CreateCBZ(string[] imagesPaths, string fileName)
 		{
-			DirectoryInfo dir = Directory.CreateDirectory(".hidden");
-			dir.Attributes = FileAttributes.Directory | FileAttributes.Hidden;
-
-			if (imagesPaths.All(f => !File.Exists(f))) 
-				throw new IOException("There is no file to add");
-
-			foreach (var image in imagesPaths)
-			{
-				var finalFilePath = image.Replace(@"\", "/").Split('/');
-				File.Copy(image, $"{dir.Name}/{finalFilePath.Last()}", true);
-			}
+			DirectoryInfo dir = CreateHiddenDir(imagesPaths);
 
 			using (var archive = ZipArchive.Create())
 			{
@@ -52,5 +42,27 @@ namespace ComicConverter
 		/// <param name="imagesPaths"></param>
 		/// <param name="fileName"></param>
 		public static void CreateCB7(string[] imagesPaths, string fileName) => throw new NotImplementedException();
+
+		/// <summary>
+		/// Simple funtion to create a hidden directory and copy all files specificataed.
+		/// </summary>
+		/// <param name="filesPaths">Files wanted to copy on the directory</param>
+		/// <returns>Directory info</returns>
+		private static DirectoryInfo CreateHiddenDir(string[] filesPaths)
+		{
+			DirectoryInfo dir = Directory.CreateDirectory(".hidden");
+			dir.Attributes = FileAttributes.Directory | FileAttributes.Hidden;
+
+			if (filesPaths.All(f => !File.Exists(f))) 
+				throw new IOException("There is no file to add");
+
+			foreach (var image in filesPaths)
+			{
+				var finalFilePath = image.Replace(@"\", "/").Split('/');
+				File.Copy(image, $"{dir.Name}/{finalFilePath.Last()}", true);
+			}
+
+			return dir;
+		}
 	}
 }
