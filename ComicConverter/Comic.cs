32c4574e58fd1ed6d0a3f49160ea19ebae722b8e
caraplana1 <cristianlandaez@gmail.com>
2022-01-05 20:53:32 -0400

    Obtain ComicFormat Method

--- ComicConverter/Comic.cs
+++ ComicConverter/Comic.cs
@@ -1,4 +1,8 @@
 using System;
+using SharpCompress.Archives.Rar;
+using SharpCompress.Archives.Zip;
+using SharpCompress.Archives.Tar;
+using SharpCompress.Archives.SevenZip;
 using System.Linq;
 using System.Collections.Generic;
 
@@ -9,7 +13,6 @@ namespace ComicConverter
 		#region Field Declaration
 
 		private readonly string comicPath;
-
 		private ComicFormat comicFormat;
 
 		#endregion
@@ -17,15 +20,42 @@ namespace ComicConverter
 		public Comic(string comicPath)
 		{
 			this.comicPath = comicPath;
+			this.comicFormat = GetComicFormat();
 		}
 
-		public Comic(string comicPath, ComicFormat comicFormat)
+		public void Convert(string outputPath, ComicFormat format)
 		{
 			this.comicPath = comicPath;
 			this.comicFormat = comicFormat;
 		}
 
-		public void Convert(string outputPath, ComicFormat outputFormat)
+		private ComicFormat GetComicFormat()
+		{
+			string[] comicpathComponents = this.comicPath.Split('.');
+
+			if (RarArchive.IsRarFile(comicPath))
+				return ComicFormat.CBR;
+			if (ZipArchive.IsZipFile(comicPath))
+				return ComicFormat.CBZ;
+			if (TarArchive.IsTarFile(comicPath))
+				return ComicFormat.CBT;
+			if (SevenZipArchive.IsSevenZipFile(comicPath))
+				return ComicFormat.CB7;
+
+			string invalidFormat = null;
+
+			if (comicpathComponents.Length > 1)
+				invalidFormat = comicpathComponents.Last();
+
+			if (invalidFormat is not null)
+				throw new FormatException($"The comic cannot be used beacuse is a {invalidFormat.ToUpper()}");
+			else
+				throw new FormatException("The comic cannot be used");
+		}
+
+		}
+
+		private Action<string, string> GetExtractorImageMethod(ComicFormat format)
 		{
 			throw new NotImplementedException();
 		}
