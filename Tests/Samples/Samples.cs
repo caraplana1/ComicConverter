8b41fbcb3ee035625ada4d5a35956c0450da7b34
caraplana1 <cristianlandaez@gmail.com>
2022-01-07 18:33:17 -0400

    Class for sample paths

---
+++ Tests/Samples/Samples.cs
@@ -0,0 +1,13 @@
+namespace Test
+{
+	/// <summary>
+	/// Samples Paths to files
+	/// </summary>
+	public static class Samples
+	{
+		public const string IMAGESDIR = "../../../Samples/Images";
+		public const string CBZPATH = "../../../Samples/Crossed 3D.cbz";
+		public const string CBRPATH = "../../../Samples/CROSSED Wish you were here.cbr";
+		public const string FAKEFILEPATH = "../../../Samples/File.test";
+	}
+}
