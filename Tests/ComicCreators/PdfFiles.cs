59d91b84b5140380f7882378326e77b1ffdb057d
caraplana1 <cristianlandaez@gmail.com>
2022-01-12 16:33:31 -0400

    Pdf creator tests

---
+++ Tests/ComicCreators/PdfFiles.cs
@@ -0,0 +1,38 @@
+using Xunit;
+using System;
+using System.IO;
+using ComicConverter;
+
+namespace Test.ComicCreators
+{
+	public class PdfFiles
+	{
+		[Fact]
+		public void CreatePdf()
+		{
+			var images = Directory.GetFiles(Samples.IMAGESDIR);
+
+			ComicBuilder.CreatePdf(images, "pdftest");
+
+			Assert.True(File.Exists("pdftest.pdf"));
+
+			File.Delete("pdftest.pdf");
+		}
+
+		[Fact]
+		public void ImagesNotFound()
+		{
+			string[] images = {"Fake1.jpeg", "fake2.jpg", "fake3.jpg"};
+
+			Assert.Throws<FileNotFoundException>(() => ComicBuilder.CreatePdf(images, "pdftest"));
+		}
+
+		[Fact]
+		public void FilesAreNotImages()
+		{
+			var images = Directory.GetFiles(Samples.IMAGESDIR);
+
+			Assert.Throws<FormatException>(() => ComicBuilder.CreatePdf(images, "pdftest"));
+		}
+	}
+}
